0a4bd2d17167f15fc74393c7addf0a8fcfd5b07e
tushar-c23 <tusharc.cse@gmail.com>
2023-10-17 12:49:02 +0530

    env

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+const url = process.env.URL_BE;
+console.log(`${url}/tshirt`);
 
 export const actions = {
     default: async({request}) => {
         const data = await request.formData(); 
-        const res = await axios.post("http://localhost:3000/tshirt", {
+        const res = await axios.post(`${url}/tshirt`, {
             email: data.get("email"),
             size: data.get("size")
         });
